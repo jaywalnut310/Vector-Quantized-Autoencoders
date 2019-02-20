b6e275853ca02bc99375dcf66e973a405c229dc7
Jaehyeon Kim <jaywalnut310@gmail.com>
2019-02-20 10:09:23 +0900

    fit indentation

--- decode.py
+++ decode.py
@@ -9,40 +9,40 @@ import transformer_vq as model
 
 
 def main():
-    parser = utils.get_argument_parser("Decode by using the trained model")
-    parser.add_argument("--checkpoint", dest="checkpoint", help="Path to a checkpoint file. Default is the latest version.")
-    parser.add_argument("--limit", type=int, default=0, help="The number of sentences to be decoded. (default=unlimited)")
-    parser.add_argument("--use_eval", action="store_true", help="Use evaluation dataset for prediction")
-    parser.add_argument("--predict_file", type=str, default="", help="Path to a text file to be translated")
-    parser.add_argument("--out_file", type=str, default="", help="Path to a text file to write")
-    args = parser.parse_args()
+  parser = utils.get_argument_parser("Decode by using the trained model")
+  parser.add_argument("--checkpoint", dest="checkpoint", help="Path to a checkpoint file. Default is the latest version.")
+  parser.add_argument("--limit", type=int, default=0, help="The number of sentences to be decoded. (default=unlimited)")
+  parser.add_argument("--use_eval", action="store_true", help="Use evaluation dataset for prediction")
+  parser.add_argument("--predict_file", type=str, default="", help="Path to a text file to be translated")
+  parser.add_argument("--out_file", type=str, default="", help="Path to a text file to write")
+  args = parser.parse_args()
 
-    hparams = create_hparams(args.model_dir, args.configs, initialize=False)
-    utils.check_git_hash(args.model_dir)
+  hparams = create_hparams(args.model_dir, args.configs, initialize=False)
+  utils.check_git_hash(args.model_dir)
 
-    data.load_vocab(hparams)
-    if args.use_eval:
-        pipeline = data.InputPipeline(hparams.source_eval_file, None, None, tf.estimator.ModeKeys.PREDICT, hparams)
-    else:
-        pipeline = data.InputPipeline(args.predict_file, None, None, tf.estimator.ModeKeys.PREDICT, hparams)
+  data.load_vocab(hparams)
+  if args.use_eval:
+    pipeline = data.InputPipeline(hparams.source_eval_file, None, None, tf.estimator.ModeKeys.PREDICT, hparams)
+  else:
+    pipeline = data.InputPipeline(args.predict_file, None, None, tf.estimator.ModeKeys.PREDICT, hparams)
 
-    estimator = tf.estimator.Estimator(model_fn=model.build_model_fn(hparams), model_dir=args.model_dir)
+  estimator = tf.estimator.Estimator(model_fn=model.build_model_fn(hparams), model_dir=args.model_dir)
 
-    # set a file path to write
-    if args.out_file != "":
-        f = open(args.out_file, 'w')
-    else:
-        f = None
+  # set a file path to write
+  if args.out_file != "":
+    f = open(args.out_file, 'w')
+  else:
+    f = None
 
-    for i, prediction in enumerate(estimator.predict(pipeline, checkpoint_path=args.checkpoint)):
-        if args.limit and i == args.limit:
-            break
-        token_ids = prediction.tolist()
-        print(hparams.target_vocab.decode(token_ids), file=f)
-        if i % 1000 == 0:
-            tf.logging.info("write: %d", i)
+  for i, prediction in enumerate(estimator.predict(pipeline, checkpoint_path=args.checkpoint)):
+    if args.limit and i == args.limit:
+      break
+    token_ids = prediction.tolist()
+    print(hparams.target_vocab.decode(token_ids), file=f)
+    if i % 1000 == 0:
+      tf.logging.info("write: %d", i)
 
 
 if __name__ == "__main__":
-    tf.logging.set_verbosity(tf.logging.INFO)
-    main()
+  tf.logging.set_verbosity(tf.logging.INFO)
+  main()
--- train.py
+++ train.py
@@ -20,52 +20,52 @@ def tensors_to_string(hparams, N=5):
 
 
 def main(argv):
-    args = utils.parse_args("Train a transformer model")
-    utils.redirect_log_to_file(args.model_dir)
+  args = utils.parse_args("Train a transformer model")
+  utils.redirect_log_to_file(args.model_dir)
 
-    hparams = create_hparams(args.model_dir, args.configs, initialize=True)
-    utils.check_git_hash(args.model_dir)
+  hparams = create_hparams(args.model_dir, args.configs, initialize=True)
+  utils.check_git_hash(args.model_dir)
 
-    # Prepare data
-    data.load_vocab(hparams)
+  # Prepare data
+  data.load_vocab(hparams)
 
-    train_input_fn = data.InputPipeline(
-            None,
-            None,
-            hparams.record_train_file,
-            tf.estimator.ModeKeys.TRAIN,
-            hparams)
-    eval_input_fn = data.InputPipeline(
-            None,
-            None,
-            hparams.record_eval_file,
-            tf.estimator.ModeKeys.EVAL,
-            hparams)
+  train_input_fn = data.InputPipeline(
+          None,
+          None,
+          hparams.record_train_file,
+          tf.estimator.ModeKeys.TRAIN,
+          hparams)
+  eval_input_fn = data.InputPipeline(
+          None,
+          None,
+          hparams.record_eval_file,
+          tf.estimator.ModeKeys.EVAL,
+          hparams)
 
-    # Training
-    log_samples_hook = tf.train.LoggingTensorHook(
-            ['targets', 'predictions'], at_end=True, formatter=tensors_to_string(hparams))
+  # Training
+  log_samples_hook = tf.train.LoggingTensorHook(
+          ['targets', 'predictions'], at_end=True, formatter=tensors_to_string(hparams))
 
-    train_spec = tf.estimator.TrainSpec(input_fn=train_input_fn,
-            max_steps=hparams.train_steps)
-    eval_spec = tf.estimator.EvalSpec(input_fn=eval_input_fn,
-            steps=hparams.eval_steps,
-            hooks=[log_samples_hook])
+  train_spec = tf.estimator.TrainSpec(input_fn=train_input_fn,
+          max_steps=hparams.train_steps)
+  eval_spec = tf.estimator.EvalSpec(input_fn=eval_input_fn,
+          steps=hparams.eval_steps,
+          hooks=[log_samples_hook])
 
-    distribution = tf.contrib.distribute.MirroredStrategy()
-    run_config = tf.estimator.RunConfig(model_dir=args.model_dir,
-            train_distribute=distribution,
-            save_summary_steps=hparams.save_summary_steps,
-            save_checkpoints_steps=hparams.save_checkpoints_steps,
-            keep_checkpoint_max=hparams.n_checkpoints)
-    estimator = tf.estimator.Estimator(
-            model_fn=model.build_model_fn(hparams),
-            config=run_config,
-            model_dir=args.model_dir)
+  distribution = tf.contrib.distribute.MirroredStrategy()
+  run_config = tf.estimator.RunConfig(model_dir=args.model_dir,
+          train_distribute=distribution,
+          save_summary_steps=hparams.save_summary_steps,
+          save_checkpoints_steps=hparams.save_checkpoints_steps,
+          keep_checkpoint_max=hparams.n_checkpoints)
+  estimator = tf.estimator.Estimator(
+          model_fn=model.build_model_fn(hparams),
+          config=run_config,
+          model_dir=args.model_dir)
 
-    tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
+  tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
 
 
 if __name__ == "__main__":
-    tf.logging.set_verbosity(tf.logging.INFO)
-    tf.app.run()
+  tf.logging.set_verbosity(tf.logging.INFO)
+  tf.app.run()
